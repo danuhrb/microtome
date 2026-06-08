4100d12b8424c950f883fae3257b52e3edc7db15
Danny <daniel.uhrbrock@gmail.com>
2026-06-08 09:41:15 -0500

    Add ThreadPool worker threads

---
+++ src/pool.rs
@@ -0,0 +1,38 @@
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<JoinHandle<()>>,
+    tx: Option<Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new(threads: usize) -> Self {
+        assert!(threads > 0);
+        let (tx, rx) = channel::<Job>();
+        let rx = Arc::new(Mutex::new(rx));
+        let workers = (0..threads)
+            .map(|_| {
+                let rx = Arc::clone(&rx);
+                std::thread::spawn(move || worker_loop(rx))
+            })
+            .collect();
+        Self {
+            workers,
+            tx: Some(tx),
+        }
+    }
+}
+
+fn worker_loop(rx: Arc<Mutex<Receiver<Job>>>) {
+    loop {
+        let job = rx.lock().unwrap().recv();
+        match job {
+            Ok(job) => job(),
+            Err(_) => break,
+        }
+    }
+}
