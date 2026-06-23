6dacc42acbe130248f20b5df1d06274d5e71c5fe
Danny <daniel.uhrbrock@gmail.com>
2026-06-23 09:15:38 -0500

    Add TIFF tag constants

---
+++ src/tiff.rs
@@ -0,0 +1,14 @@
+pub mod tags {
+    pub const IMAGE_WIDTH: u16 = 256;
+    pub const IMAGE_LENGTH: u16 = 257;
+    pub const BITS_PER_SAMPLE: u16 = 258;
+    pub const COMPRESSION: u16 = 259;
+    pub const PHOTOMETRIC_INTERPRETATION: u16 = 262;
+    pub const IMAGE_DESCRIPTION: u16 = 270;
+    pub const SAMPLES_PER_PIXEL: u16 = 277;
+    pub const TILE_WIDTH: u16 = 322;
+    pub const TILE_LENGTH: u16 = 323;
+    pub const TILE_OFFSETS: u16 = 324;
+    pub const TILE_BYTE_COUNTS: u16 = 325;
+    pub const JPEG_TABLES: u16 = 347;
+}
